f9de10305285e97526e203791ddf871bb7bd2608
bapuraoghadge29 <bapuraoghadge29@outlook.com>
2026-08-30 00:39:19 +0530

    code

--- RetailBanking/Program.cs
+++ RetailBanking/Program.cs
@@ -27,9 +27,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins(
-                    "http://localhost:5173",
-                    "https://retailbanking.azurewebsites.net")
+                .AllowAnyOrigin()//we can provide ui url here
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
